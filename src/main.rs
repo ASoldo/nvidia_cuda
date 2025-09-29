4b49dd55723484d4242c89705c7c21d656d5259d
ASoldo <soldo.andrej@gmail.com>
2025-09-29 02:09:35 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use std::io::{Error as IoError, ErrorKind};
-
 use actix_web::{App, HttpServer};
-
 use nvidia_cuda::{gpu, grpc, http::server};
+use std::io::{Error as IoError, ErrorKind};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -12,34 +10,46 @@ async fn main() -> std::io::Result<()> {
     }
 
     let (host, port) = server::bind_address();
-    println!("HTTP listening on {host}:{port}");
+    let grpc_addr = grpc::bind_address().map_err(|e| {
+        eprintln!("Invalid gRPC bind address: {e:#}");
+        IoError::new(ErrorKind::Other, e)
+    })?;
 
-    let grpc_addr = match grpc::bind_address() {
-        Ok(addr) => addr,
-        Err(e) => {
-            eprintln!("Invalid gRPC bind address: {e:#}");
-            std::process::exit(1);
-        }
-    };
+    println!("HTTP listening on {host}:{port}");
     println!("gRPC listening on {grpc_addr}");
 
-    let http_future = HttpServer::new(|| {
+    let http_srv = HttpServer::new(|| {
         App::new()
             .app_data(server::json_config())
             .configure(server::configure_services)
     })
     .bind((host.as_str(), port))?
+    .shutdown_timeout(5)
     .run();
+    let http_handle = http_srv.handle();
 
-    let grpc_future = async move {
+    let grpc_task = tokio::spawn(async move {
         grpc::serve(grpc_addr)
             .await
             .map_err(|e| IoError::new(ErrorKind::Other, e))
-    };
+    });
 
-    match tokio::try_join!(http_future, grpc_future) {
-        Ok(_) => Ok(()),
-        Err(e) if e.kind() == ErrorKind::Interrupted => Ok(()),
-        Err(e) => Err(e),
+    tokio::select! {
+        _ = tokio::signal::ctrl_c() => {
+            eprintln!("Ctrl+C received, shutting down...");
+        }
+        res = http_srv => {
+            if let Err(e) = res {
+                eprintln!("HTTP server error: {e:#}");
+                return Err(e);
+            }
+        }
     }
+
+    http_handle.stop(true).await;
+    grpc_task.abort();
+
+    let _ = grpc_task.await;
+
+    Ok(())
 }
